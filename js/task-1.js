1c5c7831f4f9982110a7941fb26fc8147c2decf6
sergiifedorishin <151446351+sergiy-fedorishin@users.noreply.github.com>
2024-04-09 21:06:23 +0300

    html update

---
+++ js/task-1.js
@@ -0,0 +1,3 @@
+const categoriesList = document.querySelector('#categories');
+const categoriesCount = categoriesList.querySelectorAll('li.item').length;
+console.log('number of categories:', categoriesCount);
