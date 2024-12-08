62ea3a6ac9482f2b792e7c2edfab502961039f2d
sergiifedorishin <151446351+sergiy-fedorishin@users.noreply.github.com>
2024-12-08 10:56:35 +0200

    tasck3

--- js/task-2.js
+++ js/task-2.js
@@ -2,27 +2,27 @@ const gallery = document.querySelector('.gallery');
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260',
-    alt: 'Біло-чорний довгошерстий кіт',
+    alt: 'White and Black Long Fur Cat',
   },
   {
     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260',
-    alt: 'Помаранчевий та білий короп біля жовтого коропа',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260',
-    alt: 'Група коней біжить',
+    alt: 'Group of Horses Running',
   },
   {
     url: 'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    alt: 'Альпійські весняні луки',
+    alt: 'Alpine Spring Meadows',
   },
   {
     url: 'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    alt: 'Природний ландшафт',
+    alt: 'Nature Landscape',
   },
   {
     url: 'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    alt: 'Маяк, узбережжя, море',
+    alt: 'Lighthouse Coast Sea',
   },
 ];
 
--- js/task-3.js
+++ js/task-3.js
@@ -2,10 +2,10 @@ const nameInput = document.getElementById('name-input');
 const nameOutput = document.getElementById('name-output');
 
 nameInput.addEventListener('input', function () {
-  const name = nameInput.value.trim(); // Видалення пробілів з обох боків
+  const name = nameInput.value.trim();
   if (name) {
     nameOutput.textContent = `Hello, ${name}!`;
   } else {
-    nameOutput.textContent = 'Hello, Anonymous!';
+    nameOutput.textContent = `Hello, Anonymous!`;
   }
 });
